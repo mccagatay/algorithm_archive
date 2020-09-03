1317f9d8a083b633305b97e96e8e3332c77c9c0d
mccagatay <muhammet.cagatay.mc@gmail.com>
2020-09-03 19:20:23 +0300

    update

---
+++ problem-1.cpp
@@ -0,0 +1,13 @@
+/*
+  If we list all natural numbers that are the required multiples of 3 or 5 of 10, we get 3, 5, 6, and 9. 
+The sum of these floors is 23.
+
+  Find the sum of all multiples of 3 or 5 of a submitted number.
+*/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
