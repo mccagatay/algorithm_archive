0afe66189902236a7ca218559320a228b47358ac
mccagatay <muhammet.cagatay.mc@gmail.com>
2020-09-04 08:17:12 +0300

    upload

--- problem1.cpp
+++ problem1.cpp
@@ -4,10 +4,36 @@ The sum of these floors is 23.
 
   Find the sum of all multiples of 3 or 5 of a submitted number.
 */
-
+//kjnkjn
 #include <iostream>
 
+using namespace std;
+bool multiples(int x);
+
 int main() {
-    std::cout << "Hello World!";
+	
+	int number;
+	
+	cin>>number;
+	
+	int multiplesTotal=0;
+	    
+    for (int i=1;i<number;i++){
+    	if(multiples(i)){    		
+    		multiplesTotal+=i;	   		
+		}		
+	}
+	
+	cout<<"Sum of multiples of 3 and 5 ="<<multiplesTotal;
+	
     return 0;
+}
+
+bool multiples(int x){
+	if(x%3==0||x%5==0){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
